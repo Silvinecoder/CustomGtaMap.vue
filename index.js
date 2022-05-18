4b551c081d2723a3273d4dc391f65cc06936c499
Cristiana-Simoes <cristiana.simoes@versantus.co.uk>
2022-05-18 23:13:51 +0100

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
 
-dotenv.config();
+const express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+
+app.use(history());
+app.use(serveStatic(__dirname + '/dist/spa'));
+app.listen(port);
